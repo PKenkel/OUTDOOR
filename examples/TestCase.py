39d6f07f6e160db0061f9a07af267ef87d53850b
Philipp Kenkel <kenke@uni-bremen.de>
2021-05-03 16:25:39 +0200

    added new line to pools for min-max production

--- examples/TestCase.py
+++ examples/TestCase.py
@@ -30,3 +30,13 @@ outdoor.displayHeatBalanceResults(Opt,Info)
 outdoor.displayBasicResults(Opt)
 
 outdoor.save_dict_to_file(Data_Path, Info)
+
+
+# a=ts.UnitsList[6]
+
+# b=ts.UnitsList[7]
+
+
+# print(ts.decimal_set)
+# Opt.DC_SET.pprint()
+# Opt.U_DIST_SUB.pprint()
