7d1999e04b623efa0d0dcbf100383f22f9c9d7a0
Philipp Kenkel <kenke@uni-bremen.de>
2021-06-15 12:54:07 +0200

    changed testcase py

--- examples/TestCase.py
+++ examples/TestCase.py
@@ -1,17 +1,16 @@
 import sys
 import os
+import cloudpickle as pic 
 
 a = os.path.dirname(__file__)
 a = os.path.dirname(a)
 
 
-b= a + '/src'
+b = a + '/src'
 sys.path.append(b)
 
 
-
-
-import outdoor
+import outdoor 
 
 Excel_Path = "Test_Excel.xlsm"
 
@@ -31,4 +30,7 @@ outdoor.displayBasicResults(Opt)
 
 outdoor.save_dict_to_file(Data_Path, Info)
 
+outdoor.save_instanceAsFile(Opt, Data_Path)
+
+    
 
