69fe34214aa83dd407bba5d9ed711f1bdc02471c
Philipp Kenkel <kenke@uni-bremen.de>
2021-10-11 11:31:19 +0200

    changed excel for new release

--- examples/TestCase.py
+++ examples/TestCase.py
@@ -20,9 +20,20 @@ Excel_Path = "Test_Excel.xlsm"
 
 Results_Path = os.path.dirname(a) + '/outdoor_examples/results/'
 
-Data_Path =os.path.dirname(a) + '/outdoor_examples/data/'
+Data_Path = os.path.dirname(a) + '/outdoor_examples/data/'
 
 ts = outdoor.get_DataFromExcel(Excel_Path)
+
+(Opt,calc_time) = outdoor.solve_OptimizationProblem(ts, 'single','gurobi', 'local')
+
+
+
+
+current, peak = tracemalloc.get_traced_memory()
+print(f"Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB")
+
+
+
 
 # ts.add_sensi_parameters('electricity_price',10,100,2)
 # ts.add_sensi_parameters('heating_demand',5,20,4,('Heat',2200))
@@ -30,25 +41,3 @@ ts = outdoor.get_DataFromExcel(Excel_Path)
 # ts.add_sensi_parameters('capital_costs',1,4,2,2200)
 
 # (Opt,Info) = outdoor.solve_OptimizationProblem(ts, 'sensitivity', 'gurobi', 'local')
-(Opt2,Info) = outdoor.solve_OptimizationProblem(ts, 'single', 'gurobi', 'local')
-
-
-outdoor.save_caseStudy(Opt2,
-                       Info,
-                       Results_Path)
-                       
-
-# Opt2  = Opt['capital_costs',30]
-
-
-# outdoor.Save_CaseStudy(Opt2, Info, Results_Path)
-
-# outdoor.displayHeatBalanceResults(Opt2,Info)
-# outdoor.displayBasicResults(Opt2)
-
-outdoor.save_dict_to_file(Data_Path, Info)
-
-
-current, peak = tracemalloc.get_traced_memory()
-print(f"Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB")
-
