0e1b995f2c2d435588627637f0a379914f7a851f
Philipp Kenkel <kenke@uni-bremen.de>
2021-10-11 11:42:09 +0200

    changed init file

--- src/outdoor/__init__.py
+++ src/outdoor/__init__.py
@@ -1,13 +1,12 @@
 __version__ = '0.0.0'
 
+from .excel_wrapper.main import get_DataFromExcel
+
 from .outdoor_core.main.outdoor_main import solve_OptimizationProblem
+
 
 from .outdoor_core.utils.output import displayBasicResults
 from .outdoor_core.utils.output import displayHeatBalanceResults
-# from .outdoor_core.utils.saver import Save_CaseStudy, save_dict_to_file, load_dict_from_file, save_instanceAsFile
-
 from .outdoor_core.utils.save_optimization import save_caseStudy,save_dict_to_file, load_dict_from_file, save_instanceAsFile
-
-from .excel_wrapper.main import get_DataFromExcel
 
 from .outdoor_core.superstructure_sytem.superstructure import Superstructure
