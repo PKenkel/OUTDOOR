66f5bbbee6cc1da4f43d9124ef0f20b8c6695545
Philipp Kenkel <kenke@uni-bremen.de>
2022-02-07 13:15:00 +0100

    reconstruction

--- src/outdoor/__init__.py
+++ src/outdoor/__init__.py
@@ -2,11 +2,12 @@ __version__ = '0.0.0'
 
 from .excel_wrapper.main import get_DataFromExcel
 
-from .outdoor_core.main.outdoor_main import solve_OptimizationProblem
 
+from .outdoor_core.input_classes.superstructure import Superstructure
+from .outdoor_core.utils.graphical_representation import create_superstructure_flowsheet
 
-from .outdoor_core.utils.output import displayBasicResults
-from .outdoor_core.utils.output import displayHeatBalanceResults
-from .outdoor_core.utils.save_optimization import save_caseStudy,save_dict_to_file, load_dict_from_file, save_instanceAsFile
+from .outdoor_core.output_classes.analyzers.basic_analyzer import BasicModelAnalyzer
+from .outdoor_core.output_classes.analyzers.advanced_multi_analyzer import AdvancedMultiModelAnalyzer
+from .outdoor_core.output_classes.analyzers.basic_multi_analyzer import BasicMultiModelAnalyzer
 
-from .outdoor_core.superstructure_sytem.superstructure import Superstructure
+from .outdoor_core.main.superstructure_problem import SuperstructureProblem
